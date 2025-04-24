27e7b955c4a7b978fe634190baf54c03933182e7
Tien Nguyen <nht@nguyenhoatien.com>
2025-04-24 14:41:21 +0700

    feat: add CORS support to public API endpoint

--- functions/src/api/api.public.ts
+++ functions/src/api/api.public.ts
@@ -1,7 +1,10 @@
 import {onRequest} from 'firebase-functions/v2/https';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({origin: true}));
 
 app.get('/api/public/', async (req, res) => {
   res.send('Hello world!');
