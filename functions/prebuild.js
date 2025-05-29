168af3555a96a40cf09f9571e71351b7fe79acd2
Tien Nguyen <nht@nguyenhoatien.com>
2025-05-29 10:15:51 +0700

    feat: enhance prebuild scripts to support multiple app configurations and improve environment variable handling

--- functions/prebuild.js
+++ functions/prebuild.js
@@ -1,22 +1,41 @@
 const fs = require('fs');
 const toml = require('toml');
 
-const filePath = '../shopify.app.toml';
 const envPath = '../.env';
+const appPath = '../shopify.app.toml';
+const prodAppPath = '../shopify.app.prod.toml';
 
-const tomlString = fs.readFileSync(filePath, 'utf-8');
-const parsedConfig = toml.parse(tomlString);
-const envString = fs.readFileSync(envPath, 'utf-8');
-const envVariables = {};
-const lines = envString.trim().split('\n');
+const envVariables = (() => {
+  const envString = fs.readFileSync(envPath, 'utf-8');
+  const lines = envString.trim().split('\n');
+  const output = {};
 
-lines.forEach(line => {
+  lines.forEach(line => {
     const [key, value] = line.split('=');
-    if (key && value !== undefined) {
-        envVariables[key.trim()] = value.trim();
+    if (value !== undefined) {
+      output[key.trim()] = value.trim();
     }
-});
+  });
 
-envVariables.SHOPIFY_APP_URL = new URL(parsedConfig.application_url).hostname;
+  return output;
+})();
+
+const appConfig = (() => {
+  const tomlString = fs.readFileSync(appPath, 'utf-8');
+  const prodTomlString = fs.readFileSync(prodAppPath, 'utf-8');
+  const parsedConfig = toml.parse(tomlString);
+  const prodParsedConfig = toml.parse(prodTomlString);
+  if (parsedConfig.client_id === envVariables['SHOPIFY_API_KEY']) {
+    return parsedConfig;
+  } else if (prodParsedConfig.client_id === envVariables['SHOPIFY_API_KEY']) {
+    return prodParsedConfig;
+  } else {
+    throw new Error('SHOPIFY_API_KEY does not match any app configuration');
+  }
+})();
+
+envVariables.SHOPIFY_APP_URL = new URL(appConfig.application_url).hostname;
+envVariables.SMTP_USER = 'tiennguyen@meowcart.net';
+envVariables.SMTP_PASSWORD = 'lgfg duvl ykdn lgki';
 
 fs.writeFileSync(`shopify.app.json`, JSON.stringify(envVariables, null, 2));
